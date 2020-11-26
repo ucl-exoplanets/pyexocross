994207d1cb045302daf39fe549fc79215d56a30b
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-11-26 18:49:05 +0000

    Build state file and compute broadeners

---
+++ pyexocross/exomol/exomolbroads.py
@@ -0,0 +1,113 @@
+import os
+import pathlib
+
+def _filter_func(trans, label, value):
+    return trans[label] == value
+
+
+
+class ExomolBroadener:
+
+    def __init__(self, default_gamma0, default_n0,t0=298.0, p0=1.0,
+                 label_defs={},filename=None):
+
+        
+        self._t0 = t0
+        self._p0 = p0
+
+        self._ratio = 1.0
+
+        self._gamma0 = default_gamma0
+        self._n0 = default_n0
+
+        
+
+        self._label_functions = {}
+        self._broadener_functions = []
+        self._broadener_values = []
+        self._precomputed_values = []
+        self._default_gamma = 0.0
+        for k,v in label_defs.items():
+            label = k
+            self.add_new_broad_label(label,v)
+        
+        if filename is not None:
+            self.load_file(filename)
+    
+    def load_file(self, filename):
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f'No file with path {filename} exists')
+        self._species = pathlib.Path(filename).stem.split('_')[-1].strip()
+        with open(filename, 'r') as f:
+
+            for line in f:
+                label, gamma, n, *quanta = line.split()
+                gamma = float(gamma)
+                n = float(n)
+                quanta = [int(q) for q in quanta]
+                if self.label_exists(label):
+                    func = self._label_functions[label]
+                    self._broadener_functions.append(lambda trans, args=quanta, func=func: func(trans,*args))
+                    self._broadener_values.append((gamma,n))
+
+    def set_temperature_pressure(self, T, P):
+        self._precomputed_values = [self.compute_gamma(gamma,n,T,P) for gamma, n in self._broadener_values]
+        self._default_gamma = self.compute_default_gamma(T,P)
+
+
+    def label_exists(self, label):
+        return label in self._label_functions
+
+    def add_new_broad_label(self, label, label_definition):
+        from operator import and_
+        from functools import reduce
+        if label == 'a0':
+            self._label_functions['a0'] = lambda trans, *args: _filter_func(trans,'J"',args[0])
+        else:
+            self._label_functions[label] = lambda trans,*args,label_def=label_definition: reduce(and_, [_filter_func(trans,l,a) for l,a in zip(label_def,args)])
+            #self._label_functions[label] = lambda trans, **args: all(_)
+    
+    def compute_gamma(self, gamma0,n0, T, P):
+        
+        return gamma0*((self._t0/T)**n0)*(P/self._p0)*self.ratio
+
+    def compute_default_gamma(self, T,P):
+        return self.compute_gamma(self._gamma0, self._n0, T, P)
+
+    @property
+    def species(self):
+        return self._species
+
+    @property
+    def ratio(self):
+        return self._ratio
+    
+    @ratio.setter
+    def ratio(self, value):
+        self._ratio = value
+
+    
+    def gamma(self, transitions):
+        import numpy as np
+
+        gamma = np.zeros(shape=(len(transitions)))
+        filt = np.empty(shape=(len(transitions)),dtype=np.bool)
+        filt[...] = True
+        for broad,gamma_val in zip(self._broadener_functions,self._precomputed_values):
+            # Get applicable gamma
+            broad_filt = broad(transitions)
+            # See leftover transitions
+            applic = filt & broad_filt
+            # If we have anny then apply it
+            if np.any(applic):
+                gamma[applic] = gamma_val
+            # Now exclude them from the search
+            filt &= ~broad_filt
+        # If we have any leftovers, apply the default
+        if np.any(filt):
+            gamma[filt] = self._default_gamma
+
+        return gamma
+            
+
+
---
+++ pyexocross/exomol/exomolstate.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from ..constants import PLANCK, PI, KBOLTZ, SPDLIGT, c2
+
+class ExomolStates:
+
+    def __init__(self,filename, fwf_definition):
+
+        self._df = self._read_state(filename, *fwf_definition)
+        self.find_quanta_start()
+
+    def find_quanta_start(self):
+        self._start = 4
+        if 'lftime' in self._df.columns:
+            self._start += 1
+        if 'lande-g' in self._df.columns:
+            self._start += 1
+
+    @property
+    def pandasFrame(self):
+        return self._df
+
+    def _read_state(self, filename, header, width, dtype):
+        import pandas as pd
+        import numpy as np
+
+        df = pd.read_fwf(filename, widths=width, names=header)
+        
+        for key, value in dtype.items():
+            if value in (np.int64, np.float64, ):
+                d = value
+
+                df[key] = pd.to_numeric(df[key], errors='coerce')
+                if value is np.int64:
+                    d = 'Int64'
+                    df[key] = df[key].astype(d)
+        return df
+
+    def Q(self, T):
+        import numexpr as ne
+        E = self._df.E.values
+        gns = self._df.g_tot.values
+
+        return ne.evaluate('sum(gns*exp(-c2*E/T))')
+
+
+    def transition_states(self, transitions, temperature=1000, pf=None, threshold=None):
+        import numexpr as ne
+        idf = transitions.iloc[:,0]
+        idi = transitions.iloc[:,1]
+        upper = self._df.iloc[idf]
+        lower = self._df.iloc[idi]
+        if pf is None:
+            pf = self.Q(temperature)
+            
+
+
+        Aif = transitions.iloc[:,2]
+
+        transition_frame = {}
+
+        v = upper.E.values - lower.E.values
+        Aif_v = Aif.values
+        gtot_f = upper.g_tot.values
+        E_i = lower.E.values
+        T = temperature
+
+        transition_frame['vif'] = v
+        
+        transition_frame['Iif'] = \
+            ne.evaluate('gtot_f*Aif_v*exp(-c2*E_i/T)*(1-exp(-c2*v/T))/(8*PI*SPDLIGT*v*v*pf)')
+
+        transition_frame['Aif'] = Aif_v
+        
+
+
+
+        transition_frame["E'"] = upper.E.values
+        transition_frame["g_tot'"] = gtot_f
+        transition_frame["J'"] = upper.J.values
+        for col in upper.columns[self._start:]:
+            transition_frame[f"{col}'"] = getattr(upper,col).values
+
+        transition_frame['E"'] = E_i
+        transition_frame['g_tot"'] = lower.g_tot.values
+        transition_frame['J"'] = lower.J.values
+        for col in lower.columns[self._start:]:
+            transition_frame[f'{col}"'] = getattr(lower,col).values
+
+        df=pd.DataFrame(transition_frame)
+
+        if threshold is not None:
+            df = df[df.Iif > threshold]
+        
+        return df
+
+
+
+
+
+
+
+
+
