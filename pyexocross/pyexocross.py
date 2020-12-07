4a49d08c8825c5b5f0f6aef8c764f1d82d9763f5
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2020-12-07 16:31:13 +0000

    Included parallel option

--- pyexocross/pyexocross.py
+++ pyexocross/pyexocross.py
@@ -1,6 +1,14 @@
 from .linelist import Linelist
 from .voigt_functions import Voigt
 
+def parallel_voigt(args, wing_cutoff=25.0, wngrid=None):
+    v, I, gamma, doppler,count = args
+    if v is None:
+        return None,None,None
+    voigt = Voigt()
+    return voigt.voigt(wngrid, v, I, doppler, gamma,cutoff=wing_cutoff) 
+
+
 class PyExocross:
 
     def __init__(self, linelist: Linelist, compute_voigt=True):
@@ -35,8 +43,7 @@ class PyExocross:
             xsec = np.zeros_like(wngrid)
         
         for v, I, gamma, doppler,count in itera:
-            if bar is not None:
-                bar.next(n=count)
+
             if v is None or len(v) == 0:
                 continue
             if self._compute_voigt:
@@ -50,3 +57,62 @@ class PyExocross:
             xsec = np.concatenate(xsec)
 
         return wngrid, xsec
+
+    def compute_xsec_parallel(self,wngrid,T,P,with_progress=True, chunksize=10000,
+                     wing_cutoff=25.0, threshold=1e-34, max_workers=4, max_jobs=100):
+        import numpy as np
+        import concurrent.futures
+        import itertools
+        from tqdm import tqdm
+        import math
+        bar = None
+        _wngrid = np.sort(wngrid)
+
+
+        itera = self._linelist.transitions(_wngrid,T, P,wing_cutoff=wing_cutoff,
+                                                               chunksize=chunksize,
+                                                               threshold=threshold)
+
+
+        itera = itera
+
+        xsec = []
+
+        out_grid = []
+        if self._compute_voigt:
+            xsec = np.zeros_like(wngrid)
+        
+        from tqdm import tqdm
+        with tqdm() as t:
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+
+                # Schedule the first N futures.  We don't want to schedule them all
+                # at once, to avoid consuming excessive amounts of memory.
+
+                futures = {
+                    executor.submit(parallel_voigt, task, wing_cutoff, wngrid)
+                    for task in itertools.islice(itera, max_jobs)
+                }
+
+                while futures:
+                    # Wait for the next future to complete.
+                    done, futures = concurrent.futures.wait(
+                        futures, return_when=concurrent.futures.FIRST_COMPLETED
+                    )
+
+                    for fut in done:
+                        res,s,e = fut.result()
+                        t.update(1)
+                        if res is None:
+                            continue
+                        xsec[s:e] += res
+                        
+
+                    # Schedule the next set of futures.  We don't want more than N futures
+                    # in the pool at a time, to keep memory consumption down.
+                    for task in itertools.islice(itera, len(done)):
+                        futures.add(
+                            executor.submit(parallel_voigt, task, wing_cutoff, wngrid)
+                        )
+
+            return wngrid, xsec
